42a261b18fdfb8cd09fe8df1d102784b64d2747d
Risejandi Autida <risejandiautida@gmail.com>
2015-10-23 17:31:41 +0800

    modified gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,15 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var out = require('gulp-out');
+var mocha = require('gulp-mocha');
 
 gulp.task('compress', function () {
   return gulp.src("./constituent.js")
     .pipe(uglify())
     .pipe(out('./dist/{basename}.min{extension}'));
+});
+
+gulp.task('test', function() {
+  return gulp.src("./test/*.js", {read: false})
+    .pipe(mocha({reporter: 'nyan'}));
 });
